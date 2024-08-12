87e1381c2a763c1e2d10f626d5b89ccaa639f1f2
Vindevogel Victor <victor.vindevogel@epitech.eu>
2024-08-12 09:31:14 +0200

    [+] fixed bug

--- algorithms/get_interface.go
+++ algorithms/get_interface.go
@@ -5,10 +5,20 @@ import (
 	"math/rand/v2"
 )
 
+func CreateRandomFlag() string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	key := make([]rune, 16)
+	for i := range key {
+		key[i] = letters[rand.IntN(len(letters))]
+	}
+	return "flag-{" + string(key) + "}"
+}
+
 func CreateRandomKey() string {
 	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-	length := rand.IntN(13)
+	length := rand.IntN(8) + 4
 	key := make([]rune, length)
 	for i := range key {
 		key[i] = letters[rand.IntN(len(letters))]
